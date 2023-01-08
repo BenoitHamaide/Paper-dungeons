5cebfc9d6640ced2b054d3bacdf28b75994cee9c
Benoit HAMAIDE <hamaidebenoit@gmail.com>
2023-01-08 13:31:25 +0100

    commit

---
+++ php/index.php
@@ -0,0 +1,12 @@
+<?php
+//include le fichier header dans mon index
+
+include 'templates/header.tpl.php';
+
+//include le fichier main
+include 'templates/index.tpl.php';
+
+//include le fichier footer dans mon index
+
+include 'templates/footer.tpl.php';
+?>
---
+++ php/templates/header.tpl.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Paper Dungeons</title>
+    <link rel="icon" href="../extractpdf/FR_Manual_v1.1-286.png">
+    <link rel="stylesheet" href="style.css"> 
+    <link rel="stylesheet" href="../css/style.css"> 
+    <link rel="preconnect" href="https://fonts.bunny.net">
+<link href="https://fonts.bunny.net/css?family=convergence:400" rel="stylesheet" />
+</head>
+
+    <body>
+        <header>
+            <img class="logo" src="../extractpdf/FR_Manual_v1.1-009_preview_rev_1.png" alt="Logo Paper Dungeons">
+            <h1>UNE MINE D'AVENTURES !</h1>
+
+            <nav class="nav_principale">
+                <ul>
+                    <li><a href="#" target="_blank">MATÉRIEL</a></li>
+                    <li><a href="#" target="_blank">MISE EN PLACE</a></li>
+                    <li><a href="#" target="_blank">COMMENT JOUER</a></li>
+                    <li><a href="#" target="_blank">VARIANTES</a></li>
+                </ul>
+            </nav>
+
+        </header>
